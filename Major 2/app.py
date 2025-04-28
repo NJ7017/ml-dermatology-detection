cde62f5a2349d4d445f32e21b917c9202a896758
NJ7017 <nj.7017@gmail.com>
2025-04-28 17:41:47 +0530

    Changes in requirements

--- Major 2/app.py
+++ Major 2/app.py
@@ -146,4 +146,5 @@ def contact():
     return render_template("contact.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    # app.run(debug=True)
